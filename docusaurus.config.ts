412edd042836aa241b743fd82c10eaffbfd216dc
jonas1015 <jonasgsmart1015@gmail.com>
2025-02-10 11:56:10 +0300

    Update baseUrl and base path configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,6 +35,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          path: 'docs',
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/vman-tool/vman3-documentation',
