77e15d478b0b8e5b95f1a7824a4cf0bf04a870a5
jonas1015 <jonasgsmart1015@gmail.com>
2025-02-10 12:08:43 +0300

    Update baseUrl and base path configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Introduction/Foreword">
+            to="/Introduction/Foreword">
             Get Started
           </Link>
         </div>
